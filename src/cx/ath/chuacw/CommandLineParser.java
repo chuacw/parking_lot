1159ea81e715ee3143aeb75943c1698e9e12d4ad
chuacw@gmail.com <1757930+chuacw@users.noreply.github.com>
2020-01-30 21:14:27 +0800

    Added hasCommand, nextCommandArgs

--- src/cx/ath/chuacw/CommandLineParser.java
+++ src/cx/ath/chuacw/CommandLineParser.java
@@ -6,23 +6,54 @@ import java.util.Scanner;
 
 public class CommandLineParser {
 
+    public String[] parseCommand(final String Line) {
+        final String[] result = Line.split(" ");
+        return result;
+    }
+
+    private Scanner scanner;
+
+    public void initScanner(final String filename) {
+        try {
+            scanner = new Scanner(new File(filename));
+        } catch (final FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean hasCommand() {
+        if (scanner != null)
+            return scanner.hasNextLine();
+        else
+            return false;
+    }
+
+    public String[] nextCommandArgs() {
+        String line;
+        if (scanner != null)
+            line = scanner.nextLine();
+        else
+            line = "";
+        String[] cmdArgs = line.split(" ");
+        return cmdArgs;
+    }
+
     public static void main(final String[] args) {
 
         if (args.length == 0) {
             System.out.println("Input filename required.");
             return;
         }
+
+        final CommandLineParser commandlineParser = new CommandLineParser();
+
         final String filename = args[0];
-        Scanner scanner;
-        try {
-            scanner = new Scanner(new File(filename));
-            while (scanner.hasNextLine()) {
-                final String commandline = scanner.nextLine();
-                final String[] commandArgs = commandline.split(" ");
-            }
-        } catch (final FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        commandlineParser.initScanner(filename);
+
+        while (commandlineParser.hasCommand()) {
+            final String[] commandArgs = commandlineParser.nextCommandArgs();
+            
+            System.out.println("Command: " + commandArgs[0]);
         }
     }
 }
