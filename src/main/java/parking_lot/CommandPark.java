b1c58babd46903603358e241483940cd3d241773
chuacw <1757930+chuacw@users.noreply.github.com>
2020-01-31 15:19:35 +0800

    Removed AppTest, added logic for parking
    Removed AppTest, and added the logic for parking

--- src/main/java/parking_lot/CommandPark.java
+++ src/main/java/parking_lot/CommandPark.java
@@ -3,18 +3,30 @@ package parking_lot;
 import java.util.ArrayList;
 
 public class CommandPark extends BaseCommand {
-	
+
 	public static String cParkFull = "Sorry, parking lot is full";
 	public static String cAllocatedSlot = "Allocated slot number: %d";
 	protected static String cPark = "park";
-	
+
 	public CommandPark() {
 		super(CommandPark.cPark, 2);
 	}
-	
+
 	@Override
 	public String run(ArrayList<String> args) {
 		ParkingLot lParkingLot = Owner.getParkingLot();
+		// park KA-01-HH-1234 White
+		String lPlate = args.get(0);
+		String lColour = args.get(1);
+
+		if (lParkingLot.getAvailable() > 0) {
+			Car lCar = new Car(lColour, lPlate);
+			Ticket lTicket = lParkingLot.parkCar(lCar);
+			String lResult = String.format(cAllocatedSlot, lTicket.getSlotNumber());
+			return lResult;
+		} else {
+			return cParkFull;
+		}
 	}
-	
+
 }
