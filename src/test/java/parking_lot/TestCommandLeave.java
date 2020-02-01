d600aee329c61f2c729122475b54f744e3e0aa2c
chuacw <1757930+chuacw@users.noreply.github.com>
2020-02-01 08:55:26 +0800

    Renamed test file for CommandLeave

--- src/test/java/parking_lot/TestCommandLeave.java
+++ src/test/java/parking_lot/TestCommandLeave.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestLeaveParking extends TestParkingLotsBase {
+public class TestCommandLeave extends TestParkingLotsBase {
 
 	
 	private String mLeaveResult;
